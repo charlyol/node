d21d9b033ff6e388268025d33b8047b5074a5a9d
charlyol <147928338+charlyol@users.noreply.github.com>
2024-04-09 16:17:18 +0200

    test2 weather and today

--- weather.js
+++ weather.js
@@ -1,10 +1,10 @@
-let weather = require('weather-js');
+var weather = require('weather-js');
 
 // Options:
 // search:     location name or zipcode
 // degreeType: F or C
 
-weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+weather.find({search: '26100, France', degreeType: 'C'}, function(err, result) {
     if(err) console.log(err);
 
     console.log(JSON.stringify(result, null, 2));
